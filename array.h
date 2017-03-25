3ea32e01c3925c891e5b904737aeda71f98dcac4
Shin-ichi MORITA <shin1morita@gmail.com>
2017-03-26 07:47:06 +0900

    Got rid of using vectors.

--- array.h
+++ array.h
@@ -3,7 +3,6 @@
 
 #include <algorithm>
 #include <memory>
-#include <vector>
 
 #include "tree.h"
 
@@ -268,12 +267,11 @@ class t_array : public t_tree
 		}
 	};
 
-	static void f_adjust(const std::vector<t_location>& a_path, int a_delta)
+	static void f_adjust(const t_location* a_first, const t_location* a_last, int a_delta)
 	{
-		auto i = a_path.end();
-		while (i != a_path.begin()) {
-			auto p = static_cast<t_branch*>((--i)->v_node);
-			for (size_t j = i->v_index; j < p->v_size; ++j) p->v_indices[j] += a_delta;
+		while (a_first != a_last) {
+			auto p = static_cast<t_branch*>((--a_last)->v_node);
+			for (size_t j = a_last->v_index; j < p->v_size; ++j) p->v_indices[j] += a_delta;
 		}
 	}
 
@@ -289,15 +287,15 @@ class t_array : public t_tree
 			delete static_cast<t_leaf*>(a_node);
 		}
 	}
-	void f_insert_leaf(std::vector<t_location>& a_path, const T_value& a_value);
-	void f_insert_branch(std::vector<t_location>& a_path, size_t a_index, void* a_node, bool a_right);
-	void f_erase_leaf(std::vector<t_location>& a_path);
-	void f_erase_branch(std::vector<t_location>& a_path);
-	void f_at(size_t a_index, std::vector<t_location>& a_path) const
+	t_location f_insert_leaf(t_location* a_first, t_location* a_last, const T_value& a_value);
+	t_location* f_insert_branch(t_location* a_first, t_location* a_last, size_t a_index, void* a_node, bool a_right);
+	t_location f_erase_leaf(t_location* a_first, t_location* a_last);
+	t_location* f_erase_branch(t_location* a_first, t_location* a_last);
+	void f_at(size_t a_index, t_location* a_path) const
 	{
 		t_location i{v_root, a_index};
-		for (size_t n = v_depth; --n > 0;) a_path.push_back(i.f_step());
-		a_path.push_back(i);
+		for (size_t n = v_depth; --n > 0;) *a_path++ = i.f_step();
+		*a_path = i;
 	}
 	void f_dump(size_t a_level, void* a_node, const std::function<void(size_t, size_t)>& a_dump) const
 	{
@@ -425,25 +423,23 @@ public:
 	{
 		return {{const_cast<t_link*>(&v_link), 0}, 0};
 	}
-	t_mutable_iterator f_insert(t_mutable_iterator a_i, const T_value& a_value)
+	t_mutable_iterator f_insert(t_constant_iterator a_i, const T_value& a_value)
 	{
 		if (v_size++ <= 0) {
 			v_root = new t_leaf(static_cast<t_leaf*>(&v_link), a_value);
 			++v_depth;
 			return f_begin();
 		}
-		std::vector<t_location> path;
+		t_location path[v_depth + 1];
 		f_at(a_i.v_index, path);
-		f_insert_leaf(path, a_value);
-		return {path.back(), a_i.v_index};
+		return {f_insert_leaf(path, path + v_depth, a_value), a_i.v_index};
 	}
-	t_mutable_iterator f_erase(t_mutable_iterator a_i)
+	t_mutable_iterator f_erase(t_constant_iterator a_i)
 	{
-		std::vector<t_location> path;
+		t_location path[v_depth];
 		f_at(a_i.v_index, path);
-		f_erase_leaf(path);
-		if (--v_size <= 0) return f_end();
-		return {path.back(), a_i.v_index};
+		--v_size;
+		return {f_erase_leaf(path, path + v_depth), a_i.v_index};
 	}
 	t_mutable_iterator f_at(size_t a_index)
 	{
@@ -463,44 +459,42 @@ public:
 };
 
 template<typename T_value, size_t A_size>
-void t_array<T_value, A_size>::f_insert_leaf(std::vector<t_location>& a_path, const T_value& a_value)
+typename t_array<T_value, A_size>::t_location t_array<T_value, A_size>::f_insert_leaf(t_location* a_first, t_location* a_last, const T_value& a_value)
 {
-	auto p = static_cast<t_leaf*>(a_path.back().v_node);
-	size_t i = a_path.back().v_index;
-	a_path.pop_back();
+	auto p = static_cast<t_leaf*>((--a_last)->v_node);
+	size_t i = a_last->v_index;
 	if (p->v_size < A_size) {
 		p->f_insert(i, a_value);
-		if (!a_path.empty()) f_adjust(a_path, 1);
-		a_path.push_back({p, i});
+		f_adjust(a_first, a_last, 1);
+		return {p, i};
 	} else if (i < (A_size + 1) / 2) {
 		auto q = new t_leaf(i, a_value, p);
-		f_insert_branch(a_path, (A_size + 1) / 2, q, false);
-		a_path.push_back({p, i});
+		f_insert_branch(a_first, a_last, (A_size + 1) / 2, q, false);
+		return {p, i};
 	} else {
 		auto q = new t_leaf(p, i, a_value);
-		f_insert_branch(a_path, (A_size + 1) / 2, q, true);
-		a_path.push_back({q, i - (A_size + 1) / 2});
+		f_insert_branch(a_first, a_last, (A_size + 1) / 2, q, true);
+		return {q, i - (A_size + 1) / 2};
 	}
 }
 
 template<typename T_value, size_t A_size>
-void t_array<T_value, A_size>::f_insert_branch(std::vector<t_location>& a_path, size_t a_index, void* a_node, bool a_right)
+typename t_array<T_value, A_size>::t_location* t_array<T_value, A_size>::f_insert_branch(t_location* a_first, t_location* a_last, size_t a_index, void* a_node, bool a_right)
 {
-	if (a_path.empty()) {
+	if (a_first == a_last) {
 		v_root = new t_branch(v_root, a_index, a_node);
 		++v_depth;
-		a_path.push_back({v_root, a_right ? 1u : 0u});
-		return;
+		*a_last = {v_root, a_right ? 1u : 0u};
+		return ++a_last;
 	}
-	auto p = static_cast<t_branch*>(a_path.back().v_node);
-	size_t i = a_path.back().v_index;
+	auto p = static_cast<t_branch*>((--a_last)->v_node);
+	size_t i = a_last->v_index;
 	if (i > 0) a_index += p->v_indices[i - 1];
-	a_path.pop_back();
 	if (p->v_size < A_size) {
 		p->f_insert(i, a_index, a_node);
-		if (!a_path.empty()) f_adjust(a_path, 1);
+		f_adjust(a_first, a_last, 1);
 		if (a_right) ++i;
-		a_path.push_back({p, i});
+		*a_last = {p, i};
 	} else if (i < (A_size + 1) / 2) {
 		size_t index = p->v_indices[(A_size + 1) / 2 - 1];
 		//      p
@@ -510,9 +504,9 @@ void t_array<T_value, A_size>::f_insert_branch(std::vector<t_location>& a_path, 
 		//    p      q
 		//  i i i   i i
 		// x X X x x x x
-		f_insert_branch(a_path, index + 1, q, false);
+		a_last = f_insert_branch(a_first, a_last, index + 1, q, false);
 		if (a_right) ++i;
-		a_path.push_back({p, i});
+		*a_last = {p, i};
 	} else if (i > (A_size + 1) / 2) {
 		size_t index = p->v_indices[(A_size + 1) / 2];
 		//      p
@@ -522,9 +516,9 @@ void t_array<T_value, A_size>::f_insert_branch(std::vector<t_location>& a_path, 
 		//    p      q
 		//  i i i   i i
 		// x x x x X X x
-		f_insert_branch(a_path, index, q, true);
+		a_last = f_insert_branch(a_first, a_last, index, q, true);
 		if (a_right) ++i;
-		a_path.push_back({q, i - (A_size + 1) / 2 - 1});
+		*a_last = {q, i - (A_size + 1) / 2 - 1};
 	} else {
 		//      p
 		//  i i i I i
@@ -533,117 +527,114 @@ void t_array<T_value, A_size>::f_insert_branch(std::vector<t_location>& a_path, 
 		//    p      q
 		//  i i i   i i
 		// x x x X X x x
-		f_insert_branch(a_path, a_index, q, a_right);
-		a_path.push_back(a_right ? t_location{q, 0} : t_location{p, (A_size + 1) / 2});
+		a_last = f_insert_branch(a_first, a_last, a_index, q, a_right);
+		*a_last = a_right ? t_location{q, 0} : t_location{p, (A_size + 1) / 2};
 	}
+	return ++a_last;
 }
 
 template<typename T_value, size_t A_size>
-void t_array<T_value, A_size>::f_erase_leaf(std::vector<t_location>& a_path)
+typename t_array<T_value, A_size>::t_location t_array<T_value, A_size>::f_erase_leaf(t_location* a_first, t_location* a_last)
 {
-	auto p = static_cast<t_leaf*>(a_path.back().v_node);
-	size_t i = a_path.back().v_index;
-	a_path.pop_back();
-	if (a_path.empty()) {
+	auto p = static_cast<t_leaf*>((--a_last)->v_node);
+	size_t i = a_last->v_index;
+	if (a_first == a_last) {
 		if (p->v_size > 1) {
 			p->f_erase(i);
-			a_path.push_back({p, i});
+			return {p, i};
 		} else {
 			v_root = nullptr;
 			p->f_unlink();
 			--v_depth;
+			return {&v_link, 0};
 		}
-		return;
 	} else if (p->v_size > A_size / 2) {
 		p->f_erase(i);
-		f_adjust(a_path, -1);
-		a_path.push_back({p, i});
-		return;
+		f_adjust(a_first, a_last, -1);
+		return {p, i};
 	}
-	auto q = static_cast<t_branch*>(a_path.back().v_node);
-	size_t j = a_path.back().v_index;
+	auto q = static_cast<t_branch*>(a_last[-1].v_node);
+	size_t j = a_last[-1].v_index;
 	if (j > 0) {
 		auto r = static_cast<t_leaf*>(q->v_nodes[j - 1]);
 		if (r->v_size > A_size / 2) {
 			r->f_rotate(p, i);
 			--q->v_indices[j - 1];
-			f_adjust(a_path, -1);
-			a_path.push_back({p, i + 1});
-			return;
+			f_adjust(a_first, a_last, -1);
+			return {p, i + 1};
 		} else if (j >= q->v_size) {
-			a_path.back().v_index = --j;
+			a_last[-1].v_index = --j;
 			r->f_merge(p, i);
 			p->f_unlink();
-			f_erase_branch(a_path);
-			a_path.push_back({r, i + A_size / 2});
-			return;
+			f_erase_branch(a_first, a_last);
+			return {r, i + A_size / 2};
 		}
 	}
 	auto r = static_cast<t_leaf*>(q->v_nodes[j + 1]);
 	if (r->v_size > A_size / 2) {
 		p->f_rotate(i, r);
 		++q->v_indices[j];
-		f_adjust(a_path, -1);
+		f_adjust(a_first, a_last, -1);
 	} else {
 		p->f_merge(i, r);
 		r->f_unlink();
-		f_erase_branch(a_path);
+		f_erase_branch(a_first, a_last);
 	}
-	a_path.push_back({p, i});
+	return {p, i};
 }
 
 template<typename T_value, size_t A_size>
-void t_array<T_value, A_size>::f_erase_branch(std::vector<t_location>& a_path)
+typename t_array<T_value, A_size>::t_location* t_array<T_value, A_size>::f_erase_branch(t_location* a_first, t_location* a_last)
 {
-	auto p = static_cast<t_branch*>(a_path.back().v_node);
-	size_t i = a_path.back().v_index;
-	a_path.pop_back();
-	if (a_path.empty()) {
+	auto p = static_cast<t_branch*>((--a_last)->v_node);
+	size_t i = a_last->v_index;
+	if (a_first == a_last) {
 		if (p->v_size > 1) {
 			p->f_erase(i);
-			a_path.push_back({p, i});
+			*a_last++ = {p, i};
 		} else {
 			v_root = p->v_nodes[0];
 			delete p;
 			--v_depth;
 		}
-		return;
+		return a_last;
 	} else if (p->v_size > A_size / 2) {
 		p->f_erase(i);
-		f_adjust(a_path, -1);
-		a_path.push_back({p, i});
-		return;
+		f_adjust(a_first, a_last, -1);
+		*a_last = {p, i};
+		return ++a_last;
 	}
-	auto q = static_cast<t_branch*>(a_path.back().v_node);
-	size_t j = a_path.back().v_index;
+	auto q = static_cast<t_branch*>(a_last[-1].v_node);
+	size_t j = a_last[-1].v_index;
 	if (j > 0) {
 		auto r = static_cast<t_branch*>(q->v_nodes[j - 1]);
 		if (r->v_size > A_size / 2) {
 			size_t base = j > 1 ? q->v_indices[j - 2] : 0;
 			r->f_rotate(base, q->v_indices[j - 1], p, i);
-			f_adjust(a_path, -1);
-			a_path.push_back({p, i + 1});
-			return;
+			f_adjust(a_first, a_last, -1);
+			*a_last = {p, i + 1};
+			return ++a_last;
 		} else if (j >= q->v_size) {
-			a_path.back().v_index = --j;
+			a_last[-1].v_index = --j;
 			r->f_merge(j > 0 ? q->v_indices[j - 1] : 0, q->v_indices[j], p, i);
 			delete p;
-			f_erase_branch(a_path);
-			a_path.push_back({r, i + A_size / 2 + 1});
-			return;
+			a_last = f_erase_branch(a_first, a_last);
+			*a_last = {r, i + A_size / 2 + 1};
+			return ++a_last;
 		}
 	}
 	auto r = static_cast<t_branch*>(q->v_nodes[j + 1]);
 	size_t base = j > 0 ? q->v_indices[j - 1] : 0;
 	if (r->v_size > A_size / 2) {
 		p->f_rotate(i, base, q->v_indices[j], r);
-		f_adjust(a_path, -1);
+		f_adjust(a_first, a_last, -1);
 	} else {
 		p->f_merge(i, base, q->v_indices[j], r);
 		delete r;
-		f_erase_branch(a_path);
+		a_last = f_erase_branch(a_first, a_last);
 	}
-	a_path.push_back({p, i});
+	*a_last = {p, i};
+	return ++a_last;
 }
 
 }
--- test_array.cc
+++ test_array.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include <cassert>
 
 #include "array.h"
